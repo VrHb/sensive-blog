2a86bfe9e6179af43d2372ba79d89cb13d5092e3
Victor Abdukarimov <viredmi@gmail.com>
2022-09-08 08:56:29 +0300

    Fixed attributes sequence in models

--- blog/models.py
+++ blog/models.py
@@ -64,39 +64,42 @@ class Post(models.Model):
         'Tag',
         related_name='posts',
         verbose_name='Теги')
-
-    def __str__(self):
-        return self.title
-
-    def get_absolute_url(self):
-        return reverse('post_detail', args={'slug': self.slug})
+    
+    objects = PostQuerySet.as_manager()
 
     class Meta:
         ordering = ['-published_at']
         verbose_name = 'пост'
         verbose_name_plural = 'посты'
     
-    objects = PostQuerySet.as_manager()
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args={'slug': self.slug})
+
+    
 
 
 class Tag(models.Model):
     title = models.CharField('Тег', max_length=20, unique=True)
-
-    def __str__(self):
-        return self.title
-
-    def clean(self):
-        self.title = self.title.lower()
-
-    def get_absolute_url(self):
-        return reverse('tag_filter', args={'tag_title': self.slug})
+    
+    objects = TagQuerySet.as_manager()
 
     class Meta:
         ordering = ['title']
         verbose_name = 'тег'
         verbose_name_plural = 'теги'
+    
+    def __str__(self):
+        return self.title
 
-    objects = TagQuerySet.as_manager()
+    def get_absolute_url(self):
+        return reverse('tag_filter', args={'tag_title': self.slug})
+
+    def clean(self):
+        self.title = self.title.lower()
+
 
 class Comment(models.Model):
     post = models.ForeignKey(
@@ -111,12 +114,13 @@ class Comment(models.Model):
 
     text = models.TextField('Текст комментария')
     published_at = models.DateTimeField('Дата и время публикации')
-
-    def __str__(self):
-        return f'{self.author.username} under {self.post.title}'
-
+    
     class Meta:
         ordering = ['published_at']
         verbose_name = 'комментарий'
         verbose_name_plural = 'комментарии'
+
+    def __str__(self):
+        return f'{self.author.username} under {self.post.title}'
+
 
